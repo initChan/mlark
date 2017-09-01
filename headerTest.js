1330a05d7d69f23082adb4f80a9a5e5c7a8357ee
initChan <initChan@users.noreply.github.com>
2017-09-02 00:15:16 +0900

    header 및 test environment 추가

---
+++ headerTest.js
@@ -0,0 +1,6 @@
+app.get('/headers', function(req,res){
+    res.set('Content-Type','text/plain');
+    var s = '';
+    for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+    res.send(s);
+});
--- meadowlark.js
+++ meadowlark.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fortune = require('./lib/fortune.js');
 var app = express();
 
-//set up handlebars view engine
+// set up handlebars view engine
 var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -12,8 +12,16 @@ app.use(express.static(__dirname +'/public'));
 
 app.set('port', process.env.PORT || 3000);
 
+// check if the URL parameter for testing is set to 1
+app.use(function (req, res, next) {
+  console.log('req.query.test:' + req.query.test);
+  console.log('env: '+ app.get('env')+'\n');
+  res.locals.showTests = app.get('env') !== 'prodution' && req.query.test === '1';
+  next();
+});
+
 // routes
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('home');
   /*
   res.type('text/plain');
@@ -21,20 +29,35 @@ app.get('/', function(req, res) {
   */
 });
 
-
-
-
-app.get('/about', function(req, res) {
-  res.render('about', {fortune: fortune.getFortune});
+app.get('/about', function (req, res) {
+  res.render('about', {
+    fortune: fortune.getFortune(),
+    pageTestScript: '/qa/tests-about.js'
+  });
+  console.log('Rendering completed in about');
   /*
   res.type('text/plain');
   res.send('About this page here... 낫띵...');
   */
 });
 
-//custom 404 page
-app.use(function(req, res) {
-  
+app.get('/headers', function (req, res) {
+    res.set('Content-Type','text/plain');
+    var s = '';
+    for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+    res.send(s);
+});
+
+app.get('/tours/hood-river', function(req, res){
+  res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+  res.render('tours/request-group-rate');
+});
+
+// custom 404 page
+app.use(function (req, res) {
   res.status(404);
   res.render('404');
   /*
@@ -45,7 +68,7 @@ app.use(function(req, res) {
 });
 
 // custom 500 page
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500);
   res.render('500')
@@ -55,7 +78,7 @@ app.use(function(err, req, res, next){
   */
 })
 
-app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' +
-    app.get('port') + '; press Ctrl-C to terminate.');
+app.listen(app.get('port'), function (){
+  console.log ('Express started on http://localhost:' +
+    app.get ('port') + '; press Ctrl-C to terminate.');
 });
